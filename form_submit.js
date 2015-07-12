0862ce043ab4b730834f441e90e0c21884c3a6be
Joe Farish <jf@blue-global.com>
2015-07-12 09:50:29 +0100

    Use reddit search when searching by author etc.

--- form_submit.js
+++ form_submit.js
@@ -16,19 +16,53 @@ function addSiteToSearch(e) {
 
         var s = document.getElementById("search");
         var sv = s.firstChild.value;
-    	sv = "site:"+document.URL+" "+sv;
+    	
 
-        var t=document.getElementsByName("t")[0];
-        var tv=t.options[t.selectedIndex].value;
+        var ignore = ["author:", "site:", "url:", "selftext:","self:","nsfw:"];
 
-        if(tv!="all"){
-            t.name=t.name+"bs";
-        var tb=document.getElementsByName("tbs")[0];
-        var tbv=tb.options[tb.selectedIndex].value;
-        var tbv="qdr:"+tv.charAt(0);
-            console.log(tbv);
+
+        var overrideSearch=true;
+
+        for (var i in ignore) {
+            
+            if (sv.includes(ignore[i])) {
+                doOverrideSearch=false;
+            }
         }
 
+        if (overrideSearch) {
+
+
+
+            // Handle restrict search to subreddit
+
+            var r=document.getElementsByName("restrict_sr")[0];
+            var site=document.URL
+            if (r) { 
+                if (r.checked) {
+                    
+                } else {site="www.reddit.com/"}
+            }
+
+
+
+
+
+            sv = "site:"+site+" "+sv;
+
+            console.log(sv);
+
+            var t=document.getElementsByName("t")[0];
+            var tv=t.options[t.selectedIndex].value;
+
+            if(tv!="all"){
+                t.name=t.name+"bs";
+            var tb=document.getElementsByName("tbs")[0];
+            var tbv=tb.options[tb.selectedIndex].value;
+            var tbv="qdr:"+tv.charAt(0);
+            }
+
+        }
     	//s.submit();
     return false;
 }
